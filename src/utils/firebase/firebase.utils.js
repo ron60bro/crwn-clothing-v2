a81a977ba970ddf2cd850db923058609fe1dc85a
Ronal <ronalshuvamboruah@gmail.coml>
2022-07-11 16:04:45 +0530

    firebase finished

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -6,6 +6,13 @@ import{
     signInWithPopup,
     GoogleAuthProvider
 }from "firebase/auth"
+
+import {
+    getFirestore,
+    doc,
+    getDoc,
+    setDoc
+} from "firebase/firestore"
 const firebaseConfig = {
     apiKey: "AIzaSyDG_Xn3CGLBnyrsOj0NBHNlugG3FtbTSsk",
     authDomain: "crwn-clothing-db-811be.firebaseapp.com",
@@ -26,3 +33,27 @@ const firebaseConfig = {
   })
   export const auth=getAuth();
   export const signInWithGooglePopup=()=>signInWithPopup(auth,provider)
+
+  export const db=getFirestore();
+
+  export const createUserDocumentFromAuth=async(userAuth)=>{
+        const userDocRef=doc(db,"users",userAuth.uid)
+        console.log(userDocRef)
+        const userSnapshot= await getDoc(userDocRef)
+        console.log(userSnapshot)
+        // console.log(userSnapshot.exists())
+        if(!userSnapshot.exists()){
+           const{displayName,email}=userAuth ;
+           const createdAt= new Date()
+           try{
+            await setDoc(userDocRef,{
+                displayName,
+                email,
+                createdAt
+            })
+           }catch(error){
+                console.log(error,"error")
+           }
+        }
+        return userDocRef;
+  }
